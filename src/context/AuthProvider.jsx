948ca0249a5d678de4f95ea6851eaa186269f7ab
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-04-08 15:28:22 +0530

    auth provider updated

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext"
+import API from '../services/api';
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
